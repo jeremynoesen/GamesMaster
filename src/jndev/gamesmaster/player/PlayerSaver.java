f18ca57a683917f8352ea33140935b34a8adc70a
Jeremy Noesen <jeremynoesen@gmail.com>
2020-03-04 21:41:30 -0600

    Finished Player saver class

--- src/jndev/gamesmaster/player/PlayerSaver.java
+++ src/jndev/gamesmaster/player/PlayerSaver.java
@@ -1,11 +1,43 @@
 package jndev.gamesmaster.player;
 
+import jndev.gamesmaster.region.inventory.InventorySaver;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+/**
+ * saves player data
+ */
 public class PlayerSaver {
     
+    /**
+     * saves all loaded players' data
+     */
+    public static void saveAll() {
+        for(GMPlayer gmPlayer : GMPlayer.getGMPlayers()) {
+            save(gmPlayer);
+        }
+    }
+    
+    /**
+     * saves preferences, purchased gadgets, purchased music, and inventories to the player file
+     *
+     * @param gmplayer gmplayer to save data for
+     */
     public static void save(GMPlayer gmplayer) {
         YamlConfiguration playerFile = gmplayer.getPlayerData().getDataFile();
+        boolean music = gmplayer.getPreferences().isMusicPublic();
+        boolean players = gmplayer.getPreferences().arePlayersVisible();
+        boolean gadgets = gmplayer.getPreferences().canGadgetInteract();
+        boolean effects = gmplayer.getPreferences().areEffectsVisible();
+        
+        playerFile.set("preferences.public-music", music);
+        playerFile.set("preferences.players-visible", players);
+        playerFile.set("preferences.gadget-interaction", gadgets);
+        playerFile.set("preferences.effects-visible", effects);
+        gmplayer.getPlayerData().savePlayerData();
+        
+        //todo music and preferences purchased
+        
+        InventorySaver.saveAllForPlayer(gmplayer.getPlayer());
     }
     
 }
